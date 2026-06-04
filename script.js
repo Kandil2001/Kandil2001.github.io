a0ccb8b90ce135052bb600f263ed95360292f80c
Ahmed Kandil <88346222+Kandil2001@users.noreply.github.com>
2026-06-05 00:57:02 +0200

    Remove obsolete pedestrian image override

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const enhancementStyles = document.createElement('link');
-enhancementStyles.rel = 'stylesheet';
-enhancementStyles.href = document.currentScript.src.includes('/projects/') ? '../enhancements.css' : 'enhancements.css';
-document.head.appendChild(enhancementStyles);
-
-const isProjectPage = document.currentScript.src.includes('/projects/');
-const localPedestrianImage = isProjectPage
-    ? '../assets/images/pedestrian-simulation.svg?v=2'
-    : 'assets/images/pedestrian-simulation.svg?v=2';
-
-document.querySelectorAll('img[src*="Jupedsim-Evacuation-Analysis"], img[alt*="pedestrian" i]').forEach((image) => {
-    image.src = localPedestrianImage;
-    image.alt = 'Pedestrian evacuation and directional flow analysis';
-    image.onerror = null;
-});
-
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
